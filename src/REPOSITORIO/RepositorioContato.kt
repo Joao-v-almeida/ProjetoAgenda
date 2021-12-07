220849b8fa843bce6810eb50e687ec6982138f9a
Joao-v-almeida <jaovittor.almeida@gmail.com>
2021-12-07 01:27:14 -0300

    Salvando um Contato

---
+++ src/REPOSITORIO/RepositorioContato.kt
@@ -0,0 +1,18 @@
+package REPOSITORIO
+
+import ENTIDADE.EntidadeContato
+
+class RepositorioContato {
+
+    companion object { // Acessar a mesma coisa sempre (classe)
+        private val listaContatos = mutableListOf<EntidadeContato>() //Lista mutável
+
+        fun salvar(contato: EntidadeContato) {
+            listaContatos.add(contato)
+        }
+
+        fun deletar(contato: EntidadeContato) {
+
+        }
+    }
+}
